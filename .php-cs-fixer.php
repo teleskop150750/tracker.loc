83e8e8c5d36606801272149daf8a1519b9609fc0
teleskop150750 <teleskop150750gmail.com>
2022-09-13 23:37:54 +0300

    php-cs-fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * This document has been generated with
+ * https://mlocati.github.io/php-cs-fixer-configurator/#version:3.11.0|configurator
+ * you can change this configuration by importing this file.
+ */
+$config = new PhpCsFixer\Config();
+
+return $config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        '@PhpCsFixer' => true,
+        '@PhpCsFixer:risky' => true,
+        'array_indentation' => true,
+        'declare_strict_types' => true,
+        'multiline_whitespace_before_semicolons' => false,
+        'void_return' => true,
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->exclude([
+                'bootstrap/cache',
+                'bower_components',
+                'node_modules',
+                'tasks',
+                'public',
+                'bin',
+                'storage',
+                'vendor',
+            ])
+            ->in(__DIR__)
+    );
