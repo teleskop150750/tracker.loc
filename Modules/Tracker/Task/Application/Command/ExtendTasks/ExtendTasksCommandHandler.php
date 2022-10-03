23946a264cc419cd24555d113e3de64aa26e83fd
teleskop150750 <teleskop150750gmail.com>
2022-10-03 22:27:41 +0300

    ExtendTasksCommandHandler

--- Modules/Tracker/Task/Application/Command/ExtendTasks/ExtendTasksCommandHandler.php
+++ Modules/Tracker/Task/Application/Command/ExtendTasks/ExtendTasksCommandHandler.php
@@ -10,6 +10,7 @@ use Modules\Shared\Application\Command\CommandHandlerInterface;
 use Modules\Tracker\Task\Domain\Entity\Task\Task;
 use Modules\Tracker\Task\Domain\Entity\Task\ValueObject\TaskEndDate;
 use Modules\Tracker\Task\Domain\Entity\Task\ValueObject\TaskStartDate;
+use Modules\Tracker\Task\Domain\Entity\TaskRelationship\TaskRelationship;
 use Modules\Tracker\Task\Domain\Entity\TaskRelationship\ValueObject\TaskRelationshipType;
 use Modules\Tracker\Task\Domain\Repository\TaskRelationshipRepositoryInterface;
 use Modules\Tracker\Task\Domain\Repository\TaskRepositoryInterface;
@@ -17,6 +18,16 @@ use Modules\Tracker\Task\Domain\Repository\TaskRepositoryInterface;
 class ExtendTasksCommandHandler implements CommandHandlerInterface
 {
     private DateTimeImmutable $toDay;
+
+    /**
+     * @var array<string, string>
+     */
+    private array $tasksIds = [];
+
+    /**
+     * @var array<string, string>
+     */
+    private array $relationshipIds = [];
 
     public function __construct(
         private readonly EntityManagerInterface $em,
@@ -28,38 +39,31 @@ class ExtendTasksCommandHandler implements CommandHandlerInterface
 
     public function __invoke(ExtendTasksCommand $command): void
     {
-        $tasksIds = [];
         $relationships = $this->taskRelationshipRepository->getExpiredTasksRelationships();
-        $relationshipsMap = [];
+        $rightIds = [];
 
         foreach ($relationships as $relationship) {
-            $left = $relationship->getLeft();
-            $right = $relationship->getRight();
-            $type = $relationship->getType()->toNative();
+            $relationshipId = $relationship->getUuid()->getId();
 
-            $leftId = $left->getUuid()->getId();
+            if (isset($this->relationshipIds[$relationshipId])) {
+                continue;
+            }
+
+            $right = $relationship->getRight();
             $rightId = $right->getUuid()->getId();
 
-            $tasksIds[$leftId] = $leftId;
-            $tasksIds[$rightId] = $rightId;
-
-            if (!isset($relationshipsMap[$rightId])) {
-                $relationshipsMap[$rightId] = [
-                    'right' => $right,
-                    'lefts' => [],
-                ];
+            if (isset($rightIds[$rightId])) {
+                continue;
             }
-            $relationshipsMap[$rightId]['lefts'][] = [
-                'left' => $left,
-                'type' => $type,
-            ];
+
+            $rightIds[$rightId] = $rightId;
+            $currentEndDateNative = $right->getEndDate()->getDateTime()->setTime(0, 0, 0, 0);
+            $diffDays = $currentEndDateNative->diff($this->toDay)->format('%r%a');
+            $diffDays = '+'.abs((int) $diffDays);
+            $this->updateEndDateProcess($right, $diffDays);
         }
 
-        foreach ($relationshipsMap as $relationship) {
-            $this->relationshipProcess($relationship);
-        }
-
-        $tasks = $this->taskRepository->getExpiredTasks($tasksIds);
+        $tasks = $this->taskRepository->getExpiredTasks($this->tasksIds);
 
         foreach ($tasks as $task) {
             $task->setEndDate(TaskEndDate::fromNative($this->toDay));
@@ -68,62 +72,143 @@ class ExtendTasksCommandHandler implements CommandHandlerInterface
         $this->em->flush();
     }
 
-    /**
-     * @param array{
-     *     right: Task,
-     *     lefts: array<int, array {
-     *          left: Task,
-     *          type: string
-     *      }>
-     *     } $relationship
-     */
-    private function relationshipProcess(array $relationship): void
+    private function updateStartDateProcess(Task $task, string $diff): void
     {
-        $right = $relationship['right'];
+        $taskId = $task->getUuid()->getId();
+        $this->tasksIds[$taskId] = $taskId;
+        $diffNumber = (int) $diff;
+        $diffString = $diffNumber >= 0 ? '+'.abs($diffNumber) : '-'.abs($diffNumber);
 
-        $currentEndDateNative = $right->getEndDate()->getDateTime()->setTime(0, 0, 0, 0);
-        $diffDays = $currentEndDateNative->diff($this->toDay)->format('%r%a');
-        $diffDays = '+'.abs((int) $diffDays);
-        $right->setEndDate(TaskEndDate::fromNative($this->toDay));
+        $currentStartDateNative = $task->getStartDate()->getDateTime();
+        $currentStartDateNative = $currentStartDateNative->setTime(0, 0, 0, 0);
+        $newStartDate = $currentStartDateNative->modify("{$diffString} day");
+        $task->setStartDate(TaskStartDate::fromNative($newStartDate));
 
-        if (false === $right->getPublished()->toNative()) {
-            return;
+//        if (false === $task->getPublished()->toNative()) {
+//            return;
+//        }
+
+        $startStartRelationships = $this->filterRelations(
+            $this->getInverseRelationships($task),
+            [TaskRelationshipType::fromNative(TaskRelationshipType::START_START)]
+        );
+
+        foreach ($startStartRelationships as $relationship) {
+            $relationshipId = $relationship->getUuid()->getId();
+            $this->relationshipIds[$relationshipId] = $relationshipId;
+            $left = $relationship->getLeft();
+            $this->updateStartDateProcess($left, $diff);
+            $this->updateEndDateProcess($left, $diff);
         }
 
-        foreach ($relationship['lefts'] as $leftMap) {
-            $type = $leftMap['type'];
-            $left = $leftMap['left'];
+        $startEndRelationships = $this->filterRelations(
+            $this->getInverseRelationships($task),
+            [TaskRelationshipType::fromNative(TaskRelationshipType::START_END)]
+        );
 
-            if (false === $left->getPublished()->toNative()) {
-                continue;
-            }
-
-            if (TaskRelationshipType::END_START === $type) {
-                $this->updateLeft($left, $diffDays, true, true);
-            } elseif (TaskRelationshipType::END_END === $type) {
-                $this->updateLeft($left, $diffDays, false, true);
-            }
+        foreach ($startEndRelationships as $relationship) {
+            $relationshipId = $relationship->getUuid()->getId();
+            $this->relationshipIds[$relationshipId] = $relationshipId;
+            $left = $relationship->getLeft();
+            $this->updateEndDateProcess($left, $diff);
         }
     }
 
-    private function updateLeft(
-        Task $task,
-        string $diff,
-        bool $changeStartDate = false,
-        bool $changeEndDate = false
-    ): void {
-        if ($changeStartDate) {
-            $currentStartDateNative = $task->getStartDate()->getDateTime();
-            $currentStartDateNative = $currentStartDateNative->setTime(0, 0, 0, 0);
-            $newStartDate = $currentStartDateNative->modify("{$diff} day");
-            $task->setStartDate(TaskStartDate::fromNative($newStartDate));
+    private function updateEndDateProcess(Task $task, string $diff): void
+    {
+        $taskId = $task->getUuid()->getId();
+        $this->tasksIds[$taskId] = $taskId;
+        $diffNumber = (int) $diff;
+        $diffString = $diffNumber >= 0 ? '+'.abs($diffNumber) : '-'.abs($diffNumber);
+
+        $currentEndDateNative = $task->getEndDate()->getDateTime();
+        $currentEndDateNative = $currentEndDateNative->setTime(0, 0, 0, 0);
+        $newEndDate = $currentEndDateNative->modify("{$diffString} day");
+        $this->setEndDate($task, TaskEndDate::fromNative($newEndDate));
+
+//        if (false === $task->getPublished()->toNative()) {
+//            return;
+//        }
+
+        $endStartRelationships = $this->filterRelations(
+            $this->getInverseRelationships($task),
+            [TaskRelationshipType::fromNative(TaskRelationshipType::END_START)],
+        );
+
+        foreach ($endStartRelationships as $relationship) {
+            $relationshipId = $relationship->getUuid()->getId();
+            $this->relationshipIds[$relationshipId] = $relationshipId;
+            $left = $relationship->getLeft();
+            $this->updateStartDateProcess($left, $diff);
+            $this->updateEndDateProcess($left, $diff);
         }
 
-        if ($changeEndDate) {
-            $currentEndDateNative = $task->getEndDate()->getDateTime();
-            $currentEndDateNative = $currentEndDateNative->setTime(0, 0, 0, 0);
-            $newEndDate = $currentEndDateNative->modify("{$diff} day");
-            $task->setEndDate(TaskEndDate::fromNative($newEndDate));
+        $endEndRelationships = $this->filterRelations(
+            $this->getInverseRelationships($task),
+            [
+                TaskRelationshipType::fromNative(TaskRelationshipType::END_END),
+            ],
+        );
+
+        foreach ($endEndRelationships as $relationship) {
+            $relationshipId = $relationship->getUuid()->getId();
+            $this->relationshipIds[$relationshipId] = $relationshipId;
+            $left = $relationship->getLeft();
+            $this->updateEndDateProcess($left, $diff);
+        }
+    }
+
+    /**
+     * @return TaskRelationship[]
+     */
+    private function getInverseRelationships(Task $task): array
+    {
+        return $task->getInverseTaskRelationships()->toArray();
+    }
+
+    /**
+     * @param TaskRelationship[]     $relationships
+     * @param TaskRelationshipType[] $types
+     *
+     * @return TaskRelationship[]
+     */
+    private function filterRelations(array $relationships, array $types): array
+    {
+        $result = [];
+
+        foreach ($relationships as $relationship) {
+            $type = $relationship->getType();
+            if (!$this->checkType($type, $types)) {
+                continue;
+            }
+
+            $result[] = $relationship;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param TaskRelationshipType[] $types
+     */
+    private function checkType(TaskRelationshipType $type, array $types = []): bool
+    {
+        foreach ($types as $item) {
+            if ($item->sameValueAs($type)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function setEndDate(Task $task, TaskEndDate $taskEndDate): void
+    {
+        $task->setEndDate($taskEndDate);
+
+        if ($task->getEndDate()->getDateTime()->getTimestamp() < $task->getStartDate()->getDateTime()->getTimestamp()) {
+            $newStartDate = TaskStartDate::fromNative(clone $taskEndDate->getDateTime());
+            $task->setStartDate($newStartDate);
         }
     }
 }
